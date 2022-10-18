720b423429c6eac8285e92f3ab721cb449253844
Supakorn Thongbor <69066050+frongtb@users.noreply.github.com>
2022-10-19 01:14:14 +0700

    Delete lab SOLID directory

--- lab SOLID/ex2/src/ScreenPrinter.java
+++
@@ -1,7 +0,0 @@
-public class ScreenPrinter implements Printer {
-    public void print(Book book) {
-        do {
-            System.out.println(book.getCurrentPage());
-        } while (book.turnToNextPage());
-    }
-}
